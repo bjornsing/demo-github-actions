012b8c179dbfd3df2d7b1ac2847c1fbcc1265bb1
Bjorn Smedman <bjornsmedman@dyson.local>
2022-11-28 23:06:30 +0100

    Minor change to test GitHub Actions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker');
+  res.send('Hello from Docker via GitHub Actions!');
 });
 
 app.listen(8080, () => {
